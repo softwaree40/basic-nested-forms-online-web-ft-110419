fc6fe54a2c457e8cbdfeeedcfc94da0a8153ec51
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-10 02:57:12 +0000

    Done.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,4 @@
 class Person < ActiveRecord::Base
   has_many :addresses
-  accepts_nested_attributes_for :addresses
+  accepts_nested_attributes_for :addresses #this line of code building def address_attributes(attr)
 end
